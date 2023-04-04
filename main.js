eb369741df7532e80d27f848f6305d5495650fbb
Stefan Paraschiva <paraschiva.stefan99@gmail.com>
2023-04-04 19:18:57 +0200

    added function that colors divs on click

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-// This function use grid-template css property to create a grid.
+createBoard(16)
+const selectSize = document.querySelector(".btn-select")
+selectSize.addEventListener("click", function(){
+    let size = getSize()
+    createBoard(size)
+})
+// This function use grid-template css property and a for-loop to create and append a grid to the container
 function createBoard(size) {
     let board = document.querySelector(".container")
 
@@ -9,8 +15,20 @@ function createBoard(size) {
 
     for(let i = 0; i < numDivs; i++){
         let div = document.createElement("div")
+        div.addEventListener("click", function(event){
+            div.style.backgroundColor = "black"
+        })
         board.appendChild(div)
     }
 }
-
-createBoard(32)
+// This function ask the user for the size of the board
+function getSize(){
+    let input = prompt("Size of the board")
+    let message = document.querySelector("#message")
+    if(input == "" || input < 16 || input > 64){
+        message.textContent = "Please insert a number between 16 and 64"
+    } else{
+        message.textContent = ""
+        return input
+    }
+}
